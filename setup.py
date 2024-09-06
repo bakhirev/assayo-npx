f84d2a9ea384aea3e66c2838be4773a7ac053d4b
bakhirev <alexey-bakhirev@yandex.ru>
2024-09-07 00:12:19 +0300

    update

--- setup.py
+++ setup.py
@@ -1,47 +1,24 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-# For a fully annotated version of this file and what it does, see
-# https://github.com/pypa/sampleproject/blob/master/setup.py
-
-# To upload this file to PyPI you must build it then upload it:
-# python setup.py sdist bdist_wheel  # build in 'dist' folder
-# python-m twine upload dist/*  # 'twine' must be installed: 'pip install twine'
-
-
 import ast
-import io
-import os
-from glob import glob
 from setuptools import find_packages, setup
-
-DEPENDENCIES = []
-EXCLUDE_FROM_PACKAGES = []
-CURDIR = os.path.abspath(os.path.dirname(__file__))
-
-with io.open(os.path.join(CURDIR, "README.md"), "r", encoding="utf-8") as f:
-    README = f.read()
 
 setup(
     name="assayo",
     version="0.0.4",
     author="Aleksei Bakhirev",
     author_email="alexey-bakhirev@yandex.ru",
-    description="",
-    long_description=README,
-    long_description_content_type="text/markdown",
+    description="Visualization and analysis of git commit statistics. Team Lead performance tool.",
     url="https://github.com/bakhirev/assayo-npx",
-    packages=find_packages(exclude=EXCLUDE_FROM_PACKAGES),
+    packages=find_packages(exclude=[]),
     include_package_data=True,
-    keywords=[],
+    keywords=["git","stat","statistic","statistics","log","report","audit","data-visualization","commit","commits","achivments"],
     scripts=[],
     entry_points={"console_scripts": ["assayo=bin.main:main"]},
     zip_safe=False,
     package_data = {"assayo":["assayo/**/*"]},
-    install_requires=DEPENDENCIES,
     python_requires=">=3.6",
-    # license and classifier list:
-    # https://pypi.org/pypi?%3Aaction=list_classifiers
     license="License :: OSI Approved :: MIT License",
     classifiers=[
         "Programming Language :: Python",
