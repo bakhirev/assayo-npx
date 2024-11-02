3c2d4465b6900fa20af21c9a32deacbb4b803498
bakhirev <alexey-bakhirev@yandex.ru>
2024-11-02 12:09:51 +0300

    update

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ long_description = (this_directory / "README.md").read_text()
 
 setup(
     name="assayo",
-    version="0.0.14",
+    version="0.0.15",
     author="Aleksei Bakhirev",
     author_email="alexey-bakhirev@yandex.ru",
     description="Visualization and analysis of git commit statistics. Team Lead performance tool.",
